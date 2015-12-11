94c529af94dfcd4e4bf34c5e7adc42cceb2c9d49
lzxz1234 <lzxz1234@gmail.com>
2015-12-11 15:46:58 +0800

    [MOD]

--- src/main/java/com/chn/common/Castors.java
+++ src/main/java/com/chn/common/Castors.java
@@ -1,6 +1,9 @@
 package com.chn.common;
 
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 @SuppressWarnings("unchecked")
 public class Castors {
@@ -13,6 +16,8 @@ public class Castors {
         map.put(String.class, Byte.class, new StringToByte());
         map.put(String.class, int.class, new StringToInt());
         map.put(String.class, Integer.class, new StringToInt());
+        map.put(String.class, short.class, new StringToShort());
+        map.put(String.class, Short.class, new StringToShort());
         map.put(String.class, long.class, new StringToLong());
         map.put(String.class, Long.class, new StringToLong());
         map.put(String.class, float.class, new StringToFloat());
@@ -21,6 +26,7 @@ public class Castors {
         map.put(String.class, Double.class, new StringToDouble());
         map.put(String.class, boolean.class, new StringToBoolean());
         map.put(String.class, Boolean.class, new StringToBoolean());
+        map.put(String.class, Date.class, new StringToDate());
     }
     
     /**
@@ -66,37 +72,63 @@ public class Castors {
     private static class StringToByte implements Castor<String, Byte> {
         @Override
         public Byte cast(String from) {
-            return new Byte(from);
+            return StringUtils.isEmpty(from) ? null : new Byte(from);
         }
     }
     private static class StringToInt implements Castor<String, Integer> {
         @Override
         public Integer cast(String from) {
-            return new Integer(from);
+            return StringUtils.isEmpty(from) ? null : new Integer(from);
+        }
+    }
+    private static class StringToShort implements Castor<String, Short> {
+        @Override
+        public Short cast(String from) {
+            return StringUtils.isEmpty(from) ? null : new Short(from);
         }
     }
     private static class StringToLong implements Castor<String, Long> {
         @Override
         public Long cast(String from) {
-            return new Long(from);
+            return StringUtils.isEmpty(from) ? null : new Long(from);
         }
     }
     private static class StringToFloat implements Castor<String, Float> {
         @Override
         public Float cast(String from) {
-            return new Float(from);
+            return StringUtils.isEmpty(from) ? null : new Float(from);
         }
     }
     private static class StringToDouble implements Castor<String, Double> {
         @Override
         public Double cast(String from) {
-            return new Double(from);
+            return StringUtils.isEmpty(from) ? null : new Double(from);
         }
     }
     private static class StringToBoolean implements Castor<String, Boolean> {
         @Override
         public Boolean cast(String from) {
             return Boolean.valueOf(from);
+        }
+    }
+    private static class StringToDate implements Castor<String, Date> {
+        private static final String[] FORMATS = new String[] {
+            "yyy-MM-dd HH:mm:ss",
+            "yyy-MM-dd",
+        };
+        @Override
+        public Date cast(String from) {
+            for(String format : FORMATS)
+                if(tryCast(from, format) != null)
+                    return tryCast(from, format);
+            throw new RuntimeException("未识别格式 " + from + " 请添加实现");
+        }
+        private Date tryCast(String from, String format) {
+            try {
+                return StringUtils.isEmpty(from) ? null : new SimpleDateFormat(format).parse(from);
+            } catch (Exception e) {
+                return null;
+            }
         }
     }
     

d092b5b9bb31cbccfbc477eddf53fbd09e7447f8
wuyongwen <wuyongwen@gmail.com>
2016-03-01 20:48:53 +0800

    test

---
+++ src/test/java/com/chn/wx/MessageHandlerTest.java
@@ -0,0 +1,15 @@
+package com.chn.wx;
+
+import org.junit.Test;
+
+import com.chn.common.Lang;
+import com.chn.wx.ioc.provider.JsonIocProvider;
+
+public class MessageHandlerTest {
+
+	@Test
+	public void test() {
+		new JsonIocProvider(new String(Lang.loadFromClassPath("/weixin.js")));
+	}
+;
+}
